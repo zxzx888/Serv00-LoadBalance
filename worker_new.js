a3bbf09a650de11fc0000b25474a2d15d89b232c
zxzx888 <zxzx888@gmail.com>
2026-06-15 04:57:16 +0800

    Update worker_new.js

--- worker_new.js
+++ worker_new.js
@@ -213,14 +213,10 @@ async function handleRequest(request, env) {
 
         clearTimeout(timeout);
 
-      if ([502, 503, 504].includes(
-        response.status)
-         ) 
-        {
-      throw new Error(
-      `HTTP ${response.status}`
-    );
-        }
+      if (!response.ok) {
+        
+          throw new Error(`HTTP ${response.status}`);
+}
         
         for (const c of controllers) {
           if (
